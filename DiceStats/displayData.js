f0f98d9bf2c66afce6fbaa7853d6ec6d8414cf1a
Star-Squid <81830542+Star-Squid@users.noreply.github.com>
2022-09-13 14:46:56 +0100

    create layout for data

---
+++ DiceStats/displayData.js
@@ -0,0 +1,24 @@
+// const resultsField = document.getElementById('display');
+// let diceForm = document.getElementById("user_input");
+
+// function showInput() {
+//     resultsField.innerText = diceForm.value.toUpperCase();
+//     return false
+// }
+
+//uncheck advantages when a disadvantage is clicked
+function checkAdvantage(obj) {
+  if (obj.classList.contains("dis")) {
+    var advs = document.getElementsByClassName("adv");
+
+    for (var i = 0; i < advs.length; i++) {
+      advs[i].checked = false;
+    }
+  } else if (obj.classList.contains("adv")) {
+    var diss = document.getElementsByClassName("dis");
+
+    for (var i = 0; i < diss.length; i++) {
+      diss[i].checked = false;
+    }
+  }
+}
